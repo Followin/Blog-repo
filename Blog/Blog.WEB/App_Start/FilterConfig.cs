13234db3b27bbf6494fb37398c7df75796c01d73
Following <Dlike.version10@gmail.com>
2015-09-02 14:44:50 +0300

    SomethignHere

--- Blog/Blog.WEB/App_Start/FilterConfig.cs
+++ Blog/Blog.WEB/App_Start/FilterConfig.cs
@@ -9,6 +9,7 @@ namespace Blog.WEB
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            //Something here
         }
     }
 }
